b9022786da545737ef08da7e28dc46d533443784
chencichang <cichang.chen@verycloud.cn>
2020-08-29 12:33:57 +0800

    modify print to print('{}'.format())

--- douban_1018/spiders/douban.py
+++ douban_1018/spiders/douban.py
@@ -13,24 +13,24 @@ class DoubanSpider(scrapy.Spider):
     login_url = 'https://accounts.douban.com/login'
 
     def parse(self, response):
-        print 'url:',response.url
+        print('url:{}'.format(response.url))
         formdata = {'source':'index_nav',
                     'redir': 'https://www.douban.com/',
-                    'form_email':'strong0528@aliyun.com',
-                    'form_password':'xxxxxxxxx',
+                    'form_email':'jingdeshuaijiang@163.com',
+                    'form_password':'wsZqaz123!@#',
                     'remember':'on',
                     'user_login':'登录'
                     }
         captcha_url = response.xpath("//div/img[@id='captcha_image']/@src").extract()
         # captcha_url2 = response.xpath("//div/img[@id='captcha_image']/@src").get()
-        print 'captcha_url:',captcha_url
-        # print 'captcha_url2:', captcha_url2
+        print('captcha_url:{}'.format( captcha_url))
+        # print('captcha_url2:{}'.format( captcha_url2))
 
         if len(captcha_url):
             captcha = self.regonize_captcha(captcha_url[0])
             formdata['captcha-solution'] = captcha
             captcha_id = response.xpath("//input[@name='captcha-id']/@value").get()
-            print 'captcha_id:',captcha_id
+            print('captcha_id:{}'.format(captcha_id))
             formdata['captcha-id'] = captcha_id
 
         yield scrapy.FormRequest(
@@ -42,23 +42,23 @@ class DoubanSpider(scrapy.Spider):
 
     def parse_login_after(self, response):
         # 验证是否成功登录
-        print '---------denglu-------'
-        print 'response.url:',response.url
+        print('---------denglu-------')
+        print('response.url:{}'.format( response.url))
         user_name = response.xpath("//li[@class='nav-user-account']/a/span/text()").get()
-        print 'user_name:',user_name
+        print('user_name:{}'.format(user_name))
         #个人主页url:
         people_index_url = response.xpath("//div[@class='more-items']//tr[1]/td/a/@href").extract()[0]
-        print 'people_index_url:',people_index_url
+        print('people_index_url:{}'.format( people_index_url))
 
         yield scrapy.Request(people_index_url, callback=self.parse_people_index)
 
     # 登录个人主页,对个人主页进行操作
     def parse_people_index(self, response):
-        print '----登录个人主页-------'
-        print '个人主页url:',response.url
+        print('----登录个人主页-------')
+        print('个人主页url:{}'.format( response.url))
         edit_url = response.xpath("//form[@name='edit_sign']/@action").extract()[0]
         edit_url_full = 'https://www.douban.com'+ edit_url
-        print 'edit_url:',edit_url
+        print('edit_url:{}'.format(edit_url))
         ck = response.xpath("//input[@name='ck']/@value").extract()[0]
         formdata = {'ck':ck,
                     'signature':'我是最美的花朵'}
@@ -78,7 +78,7 @@ class DoubanSpider(scrapy.Spider):
             data = fp.read()
             pic = b64encode(data)
             formdata['v_pic'] = pic
-        # print 'formdata:', formdata
+        # print('formdata:{}'.format( formdata))
 
         # 该接口的appcode,需购买,填写个人的.
         appcode = 'ea63dd28efea4ca98860d2bf972bb907'
@@ -87,12 +87,12 @@ class DoubanSpider(scrapy.Spider):
 
         response = requests.post(recognize_url, data=formdata, headers=headers)
         result = response.json()
-        print result
+        print(result)
         code = result['v_code']
         # 自动识别存在识别不出来,增加人工识别
         if code == '':
             code = self.regonize_captcha_by_hand(image_url)
-        print 'code:', code
+        print('code:{}'.format( code))
         return code
 
      # 显示验证码,手动输入
@@ -100,5 +100,5 @@ class DoubanSpider(scrapy.Spider):
         urllib.urlretrieve(image_url, 'captcha.png')
         image = Image.open('captcha.png')
         image.show()
-        captcha = raw_input('请输入验证码:')
+        captcha = input('请输入验证码:')
         return captcha
